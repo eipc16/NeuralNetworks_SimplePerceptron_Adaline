2217d069b785f6a5511922895aaab19f5d91126b
eipc16 <pprzemek.312@gmail.com>
2019-10-02 16:37:06 +0200

    fixed issue with training AND perceptron

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 import numpy as np
 
+np.random.seed(1)
+
 class Perceptron():
-    def __init__(self, num_of_inputs, max_weights=0.2, threshold=1000, learning_rate=0.01, bias=True):
-        self.threshold = threshold
+    def __init__(self, num_of_inputs, max_weights=1, iterations=1000, learning_rate=0.01, bias=False):
+        self.iterations = iterations
         self.learning_rate = learning_rate
-        self.bias = bias
-
-        if self.bias:
-            num_of_inputs += 1
-
-        self.weights = np.random.rand(num_of_inputs) * max_weights * 2 - max_weights
+        self.weights = np.reshape(np.random.rand(num_of_inputs), (2, 1)) * max_weights * 2 - max_weights
+        self.threshold = 0.5
+        print(f'Init weights: {self.weights.shape}')
         
     def train(self, training_data, labels):
-        for _ in range(self.threshold):
-            for inputs, label in zip(training_data, labels):
-                prediction = self.predict(inputs)
-                scaled_error = self.learning_rate * self._error(prediction, label)
+        for _ in range(self.iterations):
+            prediction = self.predict(training_data)
+            scaled_error = self._error(labels, prediction)
+            self.weights += self.learning_rate * (training_data.T @ scaled_error)
 
-                if self.bias:
-                    self.weights[1:] -= scaled_error * inputs
-                    self.weights[0] -= scaled_error
-                else:
-                    self.weights -= scaled_error * inputs
-            
         print(f'{self.weights}')
 
     def predict(self, inputs):
         if isinstance(inputs, list):
             inputs = self._fix_inputs(inputs)
-
-        if self.bias:
-            sum = np.dot(inputs, self.weights[1:]) + self.weights[0]
-        else:
-            sum = np.dot(inputs, self.weights)
+        
+        sum = np.dot(inputs, self.weights)
 
         return self._activation(sum)
 
     def _activation(self, value):
-        return 1 if value > 0 else 0
+        value = value - self.threshold
+        return np.where(value > 0, 1, 0)
 
     def _error(self, label, prediction):
         return label - prediction
@@ -72,13 +63,13 @@ labels_or = np.array([
 #OR
 print('[OR]')
 perceptron = Perceptron(2)
-perceptron.train(training_data, labels_or)
+perceptron.train(training_data, np.reshape(labels_or, (4, 1)))
 for val in training_data:
     print(f'{val} => {perceptron.predict(val)}')
 
 #AND
 print('[AND]')
 perceptron = Perceptron(2)
-perceptron.train(training_data, labels_and)
+perceptron.train(training_data, np.reshape(labels_and, (4, 1)))
 for val in training_data:
     print(f'{val} => {perceptron.predict(val)}')
